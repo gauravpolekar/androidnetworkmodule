745711f5e45182abad6726e0e60e72f5dbe90dae
Gaurav Polekar <gaurav.polekar@gmail.com>
2016-11-17 23:21:14 +0530

    added imports

--- NetworkManager.java
+++ NetworkManager.java
@@ -1,16 +1,15 @@
 
 
 import android.content.Context;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
+import org.json.JSONArray;
 import org.json.JSONObject;
-
+import android.util.Log;
 import java.util.Map;
 
 /**
@@ -106,6 +105,6 @@ calcel request for specific request code
 */
 
 	public void cancelRequest(int requestCode){
-		getRequestQueue().calcelAll(requestCode);
+		getRequestQueue().cancelAll(requestCode);
 	}
 }
